f66b6578b47bbaa151c6d5a39f999d6428d70f72
School <School@wifi-roaming-128-4-181-26.host.udel.edu>
2016-03-07 12:52:20 -0500

    https://github.com/dneuburger/Triangle.git

---
+++ src/TriangleTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class TriangleTest extends Triangle{
+
+	protected double side1, side2, side3;
+	
+	@Test
+	public void test() {
+		Triangle test = new Triangle(3,4,5);
+		double result = test.getPerimeter();
+		assertEquals(12, result, 0);
+		result = test.getArea();
+		assertEquals(6.0, result, 0.001);
+	}
+
+}
